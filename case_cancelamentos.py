652ba24202a81fe0ac6aa6fefd084fd09fa13a58
Regis <154437495+sanregi@users.noreply.github.com>
2024-03-15 19:23:53 -0300

    Update case_cancelamentos.py

--- case_cancelamentos.py
+++ case_cancelamentos.py
@@ -1,15 +1,4 @@
 
-
-# Mark2:
-st.markdown('''
-## Case - Cancelamento de Clientes <a name="1"></a>
-''', unsafe_allow_html=True)
-
-st.markdown('''
-Você foi contratado por uma empresa com mais de 800 mil clientes para um projeto de Dados. Recentemente a empresa percebeu que da sua base total de clientes, a maioria são clientes inativos, ou seja, que já cancelaram o serviço.
-Precisando melhorar seus resultados ela quer conseguir entender os principais motivos desses cancelamentos e quais as ações mais eficientes para reduzir esse número.
-''')
-
 import pandas as pd
 import streamlit as st
 import plotly.express as px
